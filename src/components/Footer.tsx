e5666309528002ef184b479768d68fe5519b6e07
Xiangjun Shi Trofimov <ohwellaye@gmail.com>
2020-11-24 19:56:33 -0500

    spelling error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => (
   <Container>
   <Box as="footer" width={[ 1, 1/2 ]} p={[ 0, 1, 2 ]} mb={5}>
     <Paragraph width={[ 0.95, 0.8 ]}>
-    Hello, I'm Shixie, a Senior Visual Designer at IBM, currently working on the future vision of IBM[dot]com. I've spend 4 of the past 5 years at IBM crafting IBM Design Language and Carbon Design System, publishing company wide guidelines in <Link href={'https://www.ibm.com/design/language/color'}>color</Link>, <Link href={'https://www.carbondesignsystem.com/guidelines/motion/basics'}>motion</Link>, <Link href={'https://www.carbondesignsystem.com/data-visualization/chart-types'}>data visualization</Link>, and <Link href={'https://www.ibm.com/design/v1/language/experience/vrar/'}>AR/VR</Link>.
+    Hello, I'm Shixie, a Senior Visual Designer at IBM, currently working on the future vision of IBM[dot]com. I've spent 4 of the past 5 years at IBM crafting IBM Design Language and Carbon Design System, publishing company wide guidelines in <Link href={'https://www.ibm.com/design/language/color'}>color</Link>, <Link href={'https://www.carbondesignsystem.com/guidelines/motion/basics'}>motion</Link>, <Link href={'https://www.carbondesignsystem.com/data-visualization/chart-types'}>data visualization</Link>, and <Link href={'https://www.ibm.com/design/v1/language/experience/vrar/'}>AR/VR</Link>.
     </Paragraph><Paragraph width={[ 0.95, 0.8 ]}>
     Outside work, I enjoys cooking, coding, drawing, gaming, and parenting my baby boy.
     </Paragraph>
